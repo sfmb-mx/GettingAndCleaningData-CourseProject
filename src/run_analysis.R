1182fecd8dd12098bb2a4c0808ebe400645fdf6b
git <smendoza.barrera@gmail.com>
2014-07-26 21:26:56 -0500

    Training and test data frames assembled.
    Training and test labels added.
    Set name added
    Subject added

--- src/run_analysis.R
+++ src/run_analysis.R
@@ -7,9 +7,9 @@
 ## Created: Sat Jul 26 13:38:25 2014 (-0500)
 ## Version: 
 ## Package-Requires: ()
-## Last-Updated: Sat Jul 26 18:29:59 2014 (-0500)
+## Last-Updated: Sat Jul 26 21:22:16 2014 (-0500)
 ##           By: Sergio-Feliciano Mendoza-Barrera
-##     Update #: 9
+##     Update #: 83
 ## URL: 
 ## Doc URL: 
 ## Keywords: 
@@ -94,32 +94,167 @@
 ######################################################################
 ## Download de datasets
 rm(list = ls())                         # Remove all workspace data
-source("dwDataFile.R")                  # Download the dataset
 
-fileName <- "HumanActivityRecognitionUsingSmartphones.zip"
-source <-
-        "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+######################################################################
+## Code to download file data sets, use once
+## source("dwDataFile.R")                  # Download the dataset
 
-dwDataFile(fileName, source)                  # Download the dataset
+## fileName <- "HumanActivityRecognitionUsingSmartphones.zip"
+## source <-
+##         "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+
+## dwDataFile(fileName, source)                  # Download the dataset
 
 ######################################################################
 ## Summary analysis of each file
 
-## restData <- read.csv("./data/restaurants.csv")
-## head(restData, n = 3)
-## tail(restData, n = 3)
-## summary(restData)
-## str(restData)
-## quantile(restData$councilDistrict, na.rm = TRUE)
-## quantile(restData$councilDistrict, probs = c(0.5, 0.75, 0.9))
+######################################################################
+##                         Training data set
+######################################################################
+## features.txt file
+featuresFile <- "./data/features.txt"
+df_features <- read.table(featuresFile, colClasses = "character")
+dim(df_features)
+class(df_features)
+head(df_features, n = 3)
+tail(df_features, n = 3)
+summary(df_features)
+str(df_features)
+## Check for missing values
+sum(is.na(df_features))
+any(is.na(df_features))
 
-## table(restData$zipCode, useNA = "ifany")
-## table(restData$councilDistrict, restData$zipCode)
+## Subsetting features data frame to obtain a vector of names
+features <- df_features[, 2]
+class(features)
+length(features)
+head(features, n = 10)
 
-## ## Check for missing values
-## sum(is.na(restData$councilDistrict))
-## any(is.na(restData$councilDistrict))
-## all(restData$zipCode > 0)
+######################################################################
+## subject_train.txt file
+subjectTrainFile <- "./data/train/subject_train.txt"
+df_subjectTrain <- read.table(subjectTrainFile, colClasses = "numeric")
+dim(df_subjectTrain)
+class(df_subjectTrain)
+head(df_subjectTrain, n = 3)
+tail(df_subjectTrain, n = 3)
+summary(df_subjectTrain)
+str(df_subjectTrain)
+## Check for missing values
+sum(is.na(df_subjectTrain))
+any(is.na(df_subjectTrain))
+
+######################################################################
+##
+## y_train.txt file
+yTrainFile <- "./data/train/y_train.txt"
+initial <- read.table(yTrainFile, nrows = 100)
+classes <- sapply(initial, class)
+yTrainData <- read.table(yTrainFile, colClasses = classes)
+
+dim(yTrainData)
+class(yTrainData)
+head(yTrainData, n = 3)
+tail(yTrainData, n = 3)
+summary(yTrainData)
+str(yTrainData)
+## Check for missing values
+sum(is.na(yTrainData))
+any(is.na(yTrainData))
+
+######################################################################
+##
+## X_train.txt file
+XTrainFile <- "./data/train/X_train.txt"
+initial <- read.table(XTrainFile, nrows = 100)
+classes <- sapply(initial, class)
+XTrainData <- read.table(XTrainFile, colClasses = classes)
+
+dim(XTrainData)
+class(XTrainData)
+head(XTrainData, n = 3)
+tail(XTrainData, n = 3)
+summary(XTrainData)
+str(XTrainData)
+## Check for missing values
+sum(is.na(XTrainData))
+any(is.na(XTrainData))
+
+######################################################################
+## Assambling data training dataframe
+trainData <- cbind(yTrainData, df_subjectTrain, XTrainData, rep("train", nrow(yTrainData)))
+dim(trainData)
+head(trainData[1:10], 1)
+names(trainData) <- c("Training-Labels", "Subject-Train", features, "set-Name")
+
+head(trainData[1:ncol(trainData)], 1)                # Sampling result
+head(trainData[1:10], 1)
+tail(trainData[1:ncol(trainData)], 1)
+tail(trainData[1:10], 1)
+
+######################################################################
+##                         Test data set
+######################################################################
+## subject_test.txt file
+subjectTestFile <- "./data/test/subject_test.txt"
+df_subjectTest <- read.table(subjectTestFile, colClasses = "numeric")
+dim(df_subjectTest)
+class(df_subjectTest)
+head(df_subjectTest, n = 3)
+tail(df_subjectTest, n = 3)
+summary(df_subjectTest)
+str(df_subjectTest)
+## Check for missing values
+sum(is.na(df_subjectTest))
+any(is.na(df_subjectTest))
+
+######################################################################
+## y_test.txt file
+yTestFile <- "./data/test/y_test.txt"
+initial <- read.table(yTestFile, nrows = 100)
+classes <- sapply(initial, class)
+yTestData <- read.table(yTestFile, colClasses = classes)
+
+dim(yTestData)
+class(yTestData)
+head(yTestData, n = 3)
+tail(yTestData, n = 3)
+summary(yTestData)
+str(yTestData)
+## Check for missing values
+sum(is.na(yTestData))
+any(is.na(yTestData))
+
+######################################################################
+## X_test.txt file
+XTestFile <- "./data/test/X_test.txt"
+initial <- read.table(XTestFile, nrows = 100)
+classes <- sapply(initial, class)
+XTestData <- read.table(XTestFile, colClasses = classes)
+
+dim(XTestData)
+class(XTestData)
+head(XTestData, n = 3)
+tail(XTestData, n = 3)
+summary(XTestData)
+str(XTestData)
+## Check for missing values
+sum(is.na(XTestData))
+any(is.na(XTestData))
+
+######################################################################
+## Assambling data test dataframe
+testData <- cbind(yTestData, df_subjectTest, XTestData, rep("test", nrow(yTestData)))
+dim(testData)
+head(testData[1:10], 1)
+names(testData) <- c("Test-Labels", "Subject-Test", features, "set-Name")
+head(testData[1:ncol(testData)], 1)                # Sampling result
+head(testData[1:10], 1)
+tail(testData[1:ncol(testData)], 1)
+tail(testData[1:10], 1)
+
+
+
 
 ######################################################################
 ### run_analysis.R ends here
